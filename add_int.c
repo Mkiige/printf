18cdd11ccf7247e7e8846a98824610b99f68e33d
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-08-09 16:46:18 +0300

    Update add_int.c

--- add_int.c
+++ add_int.c
@@ -45,3 +45,10 @@ int add_unsigned_int(va_list ap, char *buffer, int i)
 	unsigned int num = va_arg(ap, unsigned int);
 	int j = 0;
 	char *num_str;
+	
+	num_str = int_to_str(num, 10);
+	if (!num_str)
+		return (0);
+
+	for (j = 0; num_str[j] != '\0'; j++, i++)
+		buffer[i] = num_str[j];
