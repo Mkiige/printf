407167e13fcbe72a2ec991923f148acf073fe7cf
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-08-09 21:29:33 +0300

    convert_numbers.c

--- convert_numbers.c
+++ convert_numbers.c
@@ -1,90 +1,103 @@
 #include "main.h"
 
-unsigned int convert_x(va_list args, buffer_t *output,
-		unsigned char flags, char wid, char prec, unsigned char len);
-unsigned int convert_X(va_list args, buffer_t *output,
-		unsigned char flags, char wid, char prec, unsigned char len);
+unsigned int convert_sbase(buffer_t *output, long int num, char *base,
+		unsigned char flags, char wid, char prec);
+unsigned int convert_ubase(buffer_t *output,
+		unsigned long int num, char *base,
+		unsigned char flags, char wid, char prec);
 
 /**
- * convert_x - Converts an unsigned int argument to hex using abcdef
- *             and stores it to a buffer contained in a struct.
- * @args: A va_list pointing to the argument to be converted.
+ * convert_sbase - Converts a signed long to an inputted base and stores
+ *                the result to a buffer contained in a struct.
+ * @output: A buffer_t struct containing a character array.
+ * @num: A signed long to be converted.
+ * @base: A pointer to a string containing the base to convert to.
  * @flags: Flag modifiers.
  * @wid: A width modifier.
  * @prec: A precision modifier.
- * @len: A length modifier.
- * @output: A buffer_t struct containing a character array.
  *
  * Return: The number of bytes stored to the buffer.
  */
-unsigned int convert_x(va_list args, buffer_t *output,
-		unsigned char flags, char wid, char prec, unsigned char len)
+unsigned int convert_sbase(buffer_t *output, long int num, char *base,
+		unsigned char flags, char wid, char prec)
 {
-	unsigned long int num;
-	unsigned int ret = 0;
-	char *lead = "0x", space = ' ';
+	int size;
+	char digit, pad = '0';
+	unsigned int ret = 1;
 
-	if (len == LONG)
-		num = va_arg(args, unsigned long int);
+	for (size = 0; *(base + size);)
+		size++;
+
+	if (num >= size || num <= -size)
+		ret += convert_sbase(output, num / size, base,
+				flags, wid - 1, prec - 1);
+
 	else
-		num = va_arg(args, unsigned int);
-	if (len == SHORT)
-		num = (unsigned short)num;
-
-	if (HASH_FLAG == 1 && num != 0)
-		ret += _memcpy(output, lead, 2);
-
-	if (!(num == 0 && prec == 0))
-		ret += convert_ubase(output, num, "0123456789abcdef",
-				flags, wid, prec);
-
-	if (NEG_FLAG == 1)
 	{
-		for (wid -= ret; wid > 0; wid--)
-			ret += _memcpy(output, &space, 1);
+		for (; prec > 1; prec--, wid--)
+			ret += _memcpy(output, &pad, 1);
+
+		if (NEG_FLAG == 0)
+		{
+			pad = (ZERO_FLAG == 1) ? '0' : ' ';
+			for (; wid > 1; wid--)
+				ret += _memcpy(output, &pad, 1);
+		}
 	}
+
+	digit = base[(num < 0 ? -1 : 1) * (num % size)];
+	_memcpy(output, &digit, 1);
 
 	return (ret);
 }
 
 /**
- * convert_X - Converts an unsigned int argument to hex using ABCDEF
- *             and stores it to a buffer contained in a struct.
- * @args: A va_list pointing to the argument to be converted.
+ * convert_ubase - Converts an unsigned long to an inputted base and
+ *                 stores the result to a buffer contained in a struct.
+ * @output: A buffer_t struct containing a character array.
+ * @num: An unsigned long to be converted.
+ * @base: A pointer to a string containing the base to convert to.
  * @flags: Flag modifiers.
  * @wid: A width modifier.
  * @prec: A precision modifier.
- * @len: A length modifier.
- * @output: A buffer_t struct containing a character array.
  *
  * Return: The number of bytes stored to the buffer.
  */
-unsigned int convert_X(va_list args, buffer_t *output,
-		unsigned char flags, char wid, char prec, unsigned char len)
+unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
+		unsigned char flags, char wid, char prec)
 {
-	unsigned long int num;
-	unsigned int ret = 0;
-	char *lead = "0X", space = ' ';
+	unsigned int size, ret = 1;
+	char digit, pad = '0', *lead = "0x";
 
-	if (len == LONG)
-		num = va_arg(args, unsigned long);
+	for (size = 0; *(base + size);)
+		size++;
+
+	if (num >= size)
+		ret += convert_ubase(output, num / size, base,
+				flags, wid - 1, prec - 1);
+
 	else
-		num = va_arg(args, unsigned int);
-	if (len == SHORT)
-		num = (unsigned short)num;
-
-	if (HASH_FLAG == 1 && num != 0)
-		ret += _memcpy(output, lead, 2);
-
-	if (!(num == 0 && prec == 0))
-		ret += convert_ubase(output, num, "0123456789ABCDEF",
-				flags, wid, prec);
-
-	if (NEG_FLAG == 1)
 	{
-		for (wid -= ret; wid > 0; wid--)
-			ret += _memcpy(output, &space, 1);
+		if (((flags >> 5) & 1) == 1)
+		{
+			wid -= 2;
+			prec -= 2;
+		}
+		for (; prec > 1; prec--, wid--)
+			ret += _memcpy(output, &pad, 1);
+
+		if (NEG_FLAG == 0)
+		{
+			pad = (ZERO_FLAG == 1) ? '0' : ' ';
+			for (; wid > 1; wid--)
+				ret += _memcpy(output, &pad, 1);
+		}
+		if (((flags >> 5) & 1) == 1)
+			ret += _memcpy(output, lead, 2);
 	}
+
+	digit = base[(num % size)];
+	_memcpy(output, &digit, 1);
 
 	return (ret);
 }
