315d899dcd4f707731f570bc37013083b17f27d5
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-08-09 15:47:02 +0300

    Update main.h

--- main.h
+++ main.h
@@ -5,18 +5,3 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
-
-/**
- * struct sp - Struct sp
- *
- * @sp: The specifier
- * @f: The function associated
- */
-typedef struct sp
-{
-	char sp;
-	int (*f)(va_list, char *, int);
-} sp_t;
-int _printf(const char *format, ...);
-int (*mz_func(char c))(va_list, char *, int);
-#endif
