6d721df303c3859560256c861aa9786794c29dce
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-08-09 16:48:37 +0300

    Create int_to_str.c

---
+++ int_to_str.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * int_to_str - finds the representation of a given number in a given base
+ * @num: decimal number
+ * @base: base to represent the number with (2, 8, 10 or 16)
+ *
+ * Return: string representing num in the given base. Otherwise NULL
+ */
