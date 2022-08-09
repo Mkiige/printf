d72fd060d719b169b2248a3fba350ce39ed87121
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-08-09 15:15:07 +0300

    addo

--- _printf.c
+++ _printf.c
@@ -8,3 +8,8 @@
  */
 int _printf(const char *format, ...)
 {
+	/* use character buffer to call write as few times as possible */
+	char buffer[SIZE] = {0};
+	int f_idx, b_idx;
+	int (*f)(va_list, char *, int);
+	va_list ap;
