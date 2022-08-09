ce4bcf0d4189501f57d95cd440e0aaf249605b3c
Mkiige <106949498+Mkiige@users.noreply.github.com>
2022-08-09 15:23:48 +0300

    Update add_adress.c

--- add_adress.c
+++ add_adress.c
@@ -5,37 +5,8 @@
  * as a large hexadecimal number in lowercase appended with 0x
  * @ap: va_list containing the integer to add
  * @buffer: character buffer
- * @x: index from which to start adding to buffer
+ * @i: index from which to start adding to buffer
  *
  * Return: number of characters added
  */
-int add_address(va_list ap, char *buffer, int x)
-{
-  unsigned long int num = va_arg(ap, unsigned long int);
-	int j, k = 0;
-	char *num_str;
-
-	if (!num)
-		return (copy_to_buff("(nil)", buffer, x));
-
-	num_str = int_to_str(num, 16);
-	if (!num_str)
-		return (0);
-	
-	buffer[x] = '0';
-	buffer[x + 1] = 'x';
-	x += 2;
-	k += 2;
-	
-	for (j = 0; num_str[j] != '\0'; j++, x++)
-	{
-		/* convert uppercase letters to lowercase */
-		if (num_str[j] >= 'A' && num_str[j] <= 'Z')
-			buffer[x] = num_str[j] + 32;
-		else
-			buffer[x] = num_str[j];
-	}
-
-	free(num_str);
-	return (j + k);
-}
+int add_address(va_list ap, char *buffer, int i)
