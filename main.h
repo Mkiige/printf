c6a03685757511cc5de64af32a920cad28c06ce3
Mkiige <106949498+Mkiige@users.noreply.github.com>
2022-08-09 15:47:38 +0300

    Update main.h

--- main.h
+++ main.h
@@ -5,3 +5,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+
+#define SIZE 1024000 /* buffer size */
+
+/**
+ * struct sp - Struct sp
+ *
+ * @sp: The specifier
+ * @f: The function associated
+ */
+typedef struct sp
+{
+	char sp;
+	int (*f)(va_list, char *, int);
+} sp_t;
