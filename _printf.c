83c8127d34ef9470dd498b3b964529b78ad003c8
Mkiige <106949498+Mkiige@users.noreply.github.com>
2022-08-09 12:00:40 +0300

    task0

--- _printf.c
+++ _printf.c
@@ -1,1 +1,10 @@
+#include "main.h"
 
+/**
+ * _printf - produces output according to a format
+ * @format: a character string composed of zero or more directives
+ *
+ * Return: number of characters printed. Otherwise -1
+ */
+int _printf(const char *format, ...)
+{
